351b93943bd9736acacd62ed4174d7244aae2522
Amrita Priyadarshini <amrita_priyadarshini@thbs.india.com>
2022-04-28 17:03:03 +0530

    28th April

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/60/60804e78dfc6001c1e228e2d435f95cc
@@ -0,0 +1,9 @@
+package com.mysite.main;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student ,Long> {
+
+}
--- demo-sboot/src/main/java/com/mysite/main/DemoSbootApplication.java
+++ demo-sboot/src/main/java/com/mysite/main/DemoSbootApplication.java
@@ -3,9 +3,11 @@ package com.mysite.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ComponentScan("com.mysite.main")
 @SpringBootApplication
+@EnableJpaRepositories("com.mysite.main")
 public class DemoSbootApplication {
 
 	public static void main(String[] args) {
