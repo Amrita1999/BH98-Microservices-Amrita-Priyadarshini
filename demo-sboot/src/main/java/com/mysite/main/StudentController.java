5c642bc13d903942d0cb5fb1387692ceee3534e6
Amrita Priyadarshini <amrita_priyadarshini@thbs.india.com>
2022-04-29 15:46:27 +0530

    29th April

--- demo-sboot/src/main/java/com/mysite/main/StudentController.java
+++ demo-sboot/src/main/java/com/mysite/main/StudentController.java
@@ -1,9 +1,13 @@
 package com.mysite.main;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 //@RestController
@@ -37,6 +41,21 @@ public class StudentController {
 	List<Student1> getAll(){
 				
 		return stud1Repository.findAll();
+	}
+	
+	@GetMapping("/Student1/{id}")
+	Optional<Student1> getUserById(@PathVariable Long id){
+		return stud1Repository.findById(id);
+	}
+	
+	@PostMapping("/Student1")
+	Student1 newStudent1(@RequestBody Student1 stud1){
+		return stud1Repository.save(stud1);
+	}
+	
+	@DeleteMapping("/Student1/{id}")
+	void deleteUserById(@PathVariable Long id){
+		stud1Repository.deleteById(id);
 	}
 }
 	
